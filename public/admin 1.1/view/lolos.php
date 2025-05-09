37a74e65f22b4ebed0e63e87763fb7bcb61ef47c
agungnugroho0 <agungwae205@gmail.com>
2025-05-09 13:09:24 +0700

    1.2.2 Remove deprecated admin views and menu files to streamline the application structure

--- public/admin 1.1/view/lolos.php
+++
@@ -1,7 +0,0 @@
-<?php
-require $_SERVER['DOCUMENT_ROOT'].'/hikari/autoloader.php';
-$destinasi = '/hikari';
-admin();
-
-?>
-<iframe src="<?= $destinasi?>/public/admin/view/viewlolos.php" frameborder="0" class="w-full h-[82vh]"></iframe>
