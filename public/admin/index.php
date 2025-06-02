1a2bea2610b5823201861812b2b6f56424595dc7
agungnugroho0 <agungwae205@gmail.com>
2025-06-02 18:04:45 +0700

    tes

--- public/admin/index.php
+++ public/admin/index.php
@@ -18,7 +18,7 @@
     <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Dashboard</title>
+        <title>Dashboard tes</title>
         <link rel="icon" type="image/png" href="/public/image/asset/logo.png">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
         <script src="https://cdn.tailwindcss.com"></script>
