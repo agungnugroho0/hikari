f702a762140247ce2e625b63f4923b7472218983
agungnugroho0 <agungwae205@gmail.com>
2025-06-02 18:01:47 +0700

    #

--- app/api/api_jadwal_mensetsu.php
+++ app/api/api_jadwal_mensetsu.php
@@ -3,7 +3,7 @@
 require '../../autoloader.php';
 
 // ini_set('display_errors', 1);
-error_reporting(E_ALL);
+// error_reporting(E_ALL);
 // file_put_contents('/tmp/mensetsu_debug.txt', "Masuk API Mensetsu\n", FILE_APPEND);
 
 // header('Content-Type: application/json'); // Aktifkan kembali header json
