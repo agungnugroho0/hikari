69d6aa171e634ae343b25098aa3a9d14f682c2c5
agungnugroho0 <agungwae205@gmail.com>
2025-08-01 11:22:08 +0700

    Fix: Update image path to use NAS directory for staff photos in index view

--- app/view/staff/index.php
+++ app/view/staff/index.php
@@ -9,23 +9,6 @@ $data = (new staffcontroller())->tampilstaffcontroller();
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <!-- <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>staff</title> -->
-    <!-- <script src="https://cdn.tailwindcss.com"></script> -->
-    <!-- <style>
-        @font-face{
-            font-family:'Lato';
-            src: url('../font/Lato-Regular.ttf') format('truetype');
-            font-weight: normal;
-            font-style: normal;
-        }
-        .toast {
-        opacity: 1 !important; /* bikin toast jadi full solid */
-        background-color:#3ec922 !important; 
-        color: #fff !important; /* warna teks */
-        }
-    </style> -->
 </head>
 <body>
     <a href="#" onclick="loadPageFromMenu('router.php?page=staff&act=tambah', '2')" class="p-2 bg-red-900 rounded hover:bg-red-800 transition-colors duration-700 ease-out text-white font-semibold font-[Lato] dark:bg-gray-700 dark:hover:bg-black my-4">Tambah Staff</a>
@@ -61,7 +44,7 @@ $data = (new staffcontroller())->tampilstaffcontroller();
                 <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
                     <td class="px-4 w-[60px] py-2">
                         <div class="w-10 h-10 overflow-hidden object-top rounded-full">
-                            <img src="/public/image/photos/<?= $d['foto']?>" alt="<?= $d['foto']?>" class=""> 
+                            <img src="/public/image/nas_photos/<?= $d['foto']?>" alt="<?= $d['foto']?>" class=""> 
                         </div>
                     </td>
                     <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
