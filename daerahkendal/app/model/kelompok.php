5fd074a3f2b137a48dd0816912a43dd2396f11e3
agungnugroho0 <agungwae205@gmail.com>
2025-09-22 13:50:01 +0700

    update API kelompok

--- daerahkendal/app/model/kelompok.php
+++ daerahkendal/app/model/kelompok.php
@@ -17,4 +17,35 @@ class kelompok{
         $stmt->execute();
         return $stmt->fetchAll(\PDO::FETCH_ASSOC);
     }
+
+    public function kelompokid($id){
+        $query = "SELECT * FROM " . $this->table . " WHERE id_klp = :id";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+        return $stmt->fetch(\PDO::FETCH_ASSOC);
+    }
+
+    public function tambahkelompok($id_kelompok, $nama_kelompok){
+        $query = "INSERT INTO " . $this->table . " (id_klp, nama_klp) VALUES (:id_klp, :nama_klp)";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':id_klp', $id_kelompok);
+        $stmt->bindParam(':nama_klp', $nama_kelompok);
+        return $stmt->execute();
+    }
+
+    public function updatekelompok($id, $nama_kelompok){
+        $query = "UPDATE " . $this->table . " SET nama_klp = :nama_klp WHERE id_klp = :id";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':id', $id);
+        $stmt->bindParam(':nama_klp', $nama_kelompok);
+        return $stmt->execute();
+    }
+
+    public function hapuskelompok($id){
+        $query = "DELETE FROM " . $this->table . " WHERE id_klp = :id";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':id', $id);
+        return $stmt->execute();
+    }
 }
